0d7e26c2f6ff922150104d4a116bc94861bb041c
Jonathan <toto@toto.com>
2016-06-29 22:12:56 +0200

    exposition presque fini

--- Symfony/src/ModuleGestionBundle/Form/EmplacementType.php
+++ Symfony/src/ModuleGestionBundle/Form/EmplacementType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class EmplacementType extends AbstractType
 {
@@ -17,10 +18,41 @@ class EmplacementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('position', TextType::class, array(
+            ->add('position', ChoiceType::class, array(
                 'label' => 'Position',
-                'attr' => array(
-                    'class' => 'form-control ajust')))
+                'choices'  => array(
+                            '1' => '1',
+                            '2' => '2',
+                            '3' => '3',
+                            '4' => '4',
+                            '5' => '5',
+                            '6' => '6',
+                            '7' => '7',
+                            '8' => '8',
+                            '9' => '9',
+                            '10' => '10',
+                            '11' => '11',
+                            '12' => '12',
+                            '13' => '13',
+                            '14' => '14',
+                            '15' => '15',
+                            '16' => '16',
+                            '17' => '17',
+                            '18' => '18',
+                            '19' => '19',
+                            '20' => '20',
+                            '21' => '21',
+                            '22' => '22',
+                            '23' => '23',
+                            '24' => '24',
+                            '25' => '25',
+                            '26' => '26',
+                            '27' => '27',
+                            '28' => '28',
+                            '29' => '29',
+                            '30' => '30'
+                            )
+                ))
             ->add('oeuvre', EntityType::class, array(
                                                 'class' => 'ModuleGestionBundle:Oeuvre',
                                                 'choice_label' => 'nom',
