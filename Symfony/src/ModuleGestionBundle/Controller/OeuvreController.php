75b8d12a48e45d1ba3173f4b2620c0f8e50a516b
franckDev <developpeur16700@gmail.com>
2016-07-12 02:27:15 +0200

    Upload supplémentaire d'une image dans oeuvre + suppression

--- Symfony/src/ModuleGestionBundle/Controller/OeuvreController.php
+++ Symfony/src/ModuleGestionBundle/Controller/OeuvreController.php
@@ -168,62 +168,91 @@ class OeuvreController extends Controller
                 // Puis je définis le type d'oeuvre
                 $oeuvre->setTypeOeuvre($statut);
             }
-          
+            
             //Si on a un fichier multimedia
             if($request->files->count() > 0){
 
-                // On récupère le fichier 
-                $file = $request->files->get("oeuvre")["multi"][1]["fichier"];
+                // Si on a une image de l'oeuvre
+                if(count($request->files->get("oeuvre")["image"]) > 0){
 
-                // On génère un nom unique pour ce fichier 
-                $filename = md5(uniqid()).'.'.$file->getClientOriginalExtension();
+                    // On récupère le fichier
+                    $file = $request->files->get("oeuvre")["image"];
 
-                // Fonction pour calculer la taille du fichier
-                function taille_fichier($octets) {
-                    $resultat = $octets;
-                    for ($i=0; $i < 8 && $resultat >= 1024; $i++) {
-                        $resultat = $resultat / 1024;
-                    }
-                    if ($i > 0) {
-                        return preg_replace('/,00$/', '', number_format($resultat, 2, ',', '')) 
-                . ' ' . substr('KMGTPEZY',$i-1,1) . 'o';
-                    } else {
-                        return $resultat . ' o';
-                    }
+                    // On génère un nom unique pour ce fichier 
+                    $filename = md5(uniqid()).'.'.$file->getClientOriginalExtension();
+
+                    // On déplace ensuite le fichier dans le dossier prévu à cette effet
+                    $file->move(
+                        $this->container->getParameter('multimedias_directory'),
+                        $filename
+                    );
+                    
+                    // Nom unique et nom original de l'image
+                    $image = $filename;
+                    $nom_image = pathinfo($file->getClientOriginalName())["filename"];
+                    // On définit ces deux derniers
+                    $oeuvre->setImage($image);
+                    $oeuvre->setNomImage($nom_image);
                 }
 
-                // On déplace ensuite le fichier dans le dossier prévu à cette effet
-                $file->move(
-                    $this->container->getParameter('multimedias_directory'),
-                    $filename
-                );
+                // Si on a un contenu additionnel multimédia
+                if(count($request->files->get("oeuvre")["multi"]) > 0){
 
-                $titre = pathinfo($file->getClientOriginalName())["filename"];
-                $dateCreation = $request->request->all()["oeuvre"]["multi"][1]["multi"]["dateCreation"];
-                $duree = $request->request->all()["oeuvre"]["multi"][1]["duree"];
-                $stockage = taille_fichier($file->getClientSize());
-                // Si on a une video
-                if(isset($request->request->all()["oeuvre"]["multi"][1]["video"]))
-                    $video = $request->request->all()["oeuvre"]["multi"][1]["video"];
-                else
-                    $video = 0;
+                    // On récupère le fichier 
+                    $file = $request->files->get("oeuvre")["multi"][1]["fichier"];
 
-                // On instancie un objet MultimediaType
-                $multimedia =  new MultimediaType();
-                $multimedia->setTitre($titre);
-                $multimedia->setDateCreation($dateCreation);
-                $multimedia->setDuree($duree);
-                $multimedia->setStockage($stockage);
-                $multimedia->setVideo($video);
-                $multimedia->setFichier($filename);
+                    // On génère un nom unique pour ce fichier 
+                    $filename = md5(uniqid()).'.'.$file->getClientOriginalExtension();
 
-                // Je persiste le multimedia et je l'enregistre
-                $em->persist($multimedia);
-                $em->flush();
+                    // Fonction pour calculer la taille du fichier
+                    function taille_fichier($octets) {
+                        $resultat = $octets;
+                        for ($i=0; $i < 8 && $resultat >= 1024; $i++) {
+                            $resultat = $resultat / 1024;
+                        }
+                        if ($i > 0) {
+                            return preg_replace('/,00$/', '', number_format($resultat, 2, ',', '')) 
+                    . ' ' . substr('KMGTPEZY',$i-1,1) . 'o';
+                        } else {
+                            return $resultat . ' o';
+                        }
+                    }
 
-                // Puis je définis le type d'oeuvre
-                $oeuvre->setTypeOeuvre($multimedia);
+                    // On déplace ensuite le fichier dans le dossier prévu à cette effet
+                    $file->move(
+                        $this->container->getParameter('multimedias_directory'),
+                        $filename
+                    );
+
+                    $titre = pathinfo($file->getClientOriginalName())["filename"];
+                    $dateCreation = $request->request->all()["oeuvre"]["multi"][1]["multi"]["dateCreation"];
+                    $duree = $request->request->all()["oeuvre"]["multi"][1]["duree"];
+                    $stockage = taille_fichier($file->getClientSize());
+                    // Si on a une video
+                    if(isset($request->request->all()["oeuvre"]["multi"][1]["video"]))
+                        $video = $request->request->all()["oeuvre"]["multi"][1]["video"];
+                    else
+                        $video = 0;
+
+                    // On instancie un objet MultimediaType
+                    $multimedia =  new MultimediaType();
+                    $multimedia->setTitre($titre);
+                    $multimedia->setDateCreation($dateCreation);
+                    $multimedia->setDuree($duree);
+                    $multimedia->setStockage($stockage);
+                    $multimedia->setVideo($video);
+                    $multimedia->setFichier($filename);
+
+                    // Je persiste le multimedia et je l'enregistre
+                    $em->persist($multimedia);
+                    $em->flush();
+
+                    // Puis je définis le type d'oeuvre
+                    $oeuvre->setTypeOeuvre($multimedia);
+                }
+
             }
+
             $em->persist($oeuvre);
             $em->flush();
 
@@ -419,6 +448,17 @@ class OeuvreController extends Controller
                     $fs = new Filesystem();
                     // On récupère le nom exact du fichier à supprimer
                     $symlink = $oeuvre->getTypeOeuvre()->getFichier();
+                    // On définit le chemin de la suppression du fichier
+                    $path = $this->container->getParameter('multimedias_directory')."/".$symlink;
+                    // On supprime
+                    $fs->remove($path);
+                }
+                // Si il y a une image avec l'oeuvre
+                if($oeuvre->getImage() != null){
+                    // On instancie un objet fichier system
+                    $fs = new Filesystem();
+                    // On récupère le nom exact du fichier à supprimer
+                    $symlink = $oeuvre->getImage();
                     // On définit le chemin de la suppression du fichier
                     $path = $this->container->getParameter('multimedias_directory')."/".$symlink;
                     // On supprime
