574ea4b93a00e84281bef67931c0a9b23354f55d
franckDev <developpeur16700@gmail.com>
2016-07-24 01:42:05 +0200

    maj

--- Symfony/src/ModuleDiffusionBundle/Controller/AccueilController.php
+++ Symfony/src/ModuleDiffusionBundle/Controller/AccueilController.php
@@ -4,6 +4,8 @@ namespace ModuleDiffusionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ModuleGestionBundle\Entity\Exposition;
+use ModuleGestionBundle\Entity\Oeuvre;
+use ModuleGestionBundle\Entity\Emplacement;
 
 /**
  * Accueil controller.
@@ -13,7 +15,19 @@ class AccueilController extends Controller
 {
 	public function indexAction()
 	{
-		
-		return $this->render('ModuleDiffusion/accueil/index.html.twig');
+		//Connection à la base de données
+        $connection = $this->get('database_connection');
+
+        // récupérer la liste complète des oeuvres
+        $query = "select e.nomExposition as nomEx, oe.nom as nomOe from Exposition as e
+        		  inner join Emplacement as em on e.id=em.exposition_id
+        		  inner join Oeuvre as oe on oe.id=em.oeuvre_id
+        		  where e.id=21";
+
+        // On stocke le résultat
+        $rows = $connection->fetchAll($query);
+
+		return $this->render('ModuleDiffusion/accueil/index.html.twig', array(
+			'rows' => $rows));
 	}
 }
