4cce11fbf33b61e56d076a5fc7738e86ca93e0f9
Jonathan <toto@toto.com>
2016-07-27 13:51:48 +0200

    Print expo

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -23,7 +23,7 @@ class AppKernel extends Kernel
             new Ob\HighchartsBundle\ObHighchartsBundle,
             new ModuleDiffusionBundle\ModuleDiffusionBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new ADesigns\CalendarBundle\ADesignsCalendarBundle()
+            new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
